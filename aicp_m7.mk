a6104b2a94083f23407267c11dddbe5b39db6aba
Julian Veit <claymore1298@gmail.com>
2020-04-19 10:55:02 +0200

    m7: add back fingerprint

--- aicp_m7.mk
+++ aicp_m7.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2020 Android Ice Cold Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,3 +25,6 @@ PRODUCT_DEVICE := m7
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_MODEL := One
 PRODUCT_NAME := aicp_m7
+
+BUILD_FINGERPRINT="htc/m7_google/m7:5.1/LMY47O.H18/666675:user/release-keys"
+
